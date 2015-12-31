c44719d46d5429a6cec760a833b67bdd007edddc
qiang53053857 <xiaoqiang857@163.com>
2015-12-31 17:55:52 +0800

    add msbuild
    database table

---
+++ MBMS/MBMS.DB/log/Tables/Order.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [From] INT NOT NULL, 
+    [To] INT NOT NULL, 
+    [Goods] NVARCHAR(500) NULL, 
+    [Count] FLOAT NULL, 
+    [UnitPrice] FLOAT NULL, 
+    [TotalPrice] FLOAT NOT NULL, 
+    [Status] INT NOT NULL
+)
---
+++ MBMS/MBMS.DB/log/Tables/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Order]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] NVARCHAR(500) NOT NULL, 
+    [Password] NVARCHAR(500) NOT NULL, 
+    [Nickname] NVARCHAR(500) NOT NULL, 
+    [Icon] NVARCHAR(500) NULL, 
+    [Superior] NVARCHAR(MAX) NULL, 
+    [Subordinate] NVARCHAR(MAX) NULL
+)
